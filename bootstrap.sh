3f13e21584515317c41b774ec86d38ec314d4cdf
NikolaMandic <mn080202@gmail.com>
2016-02-15 21:52:07 +0200

    ads

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+cd ~
+sudo apt-get install -y git build-essential
+git init
+git remote add origin https://github.com/NikolaMandic/houmdajrektori.git
+git fetch origin
+git checkout master
+git submodule update --init --recursive
+cd .vim/bundle/vimproc
+make
+cd ~
