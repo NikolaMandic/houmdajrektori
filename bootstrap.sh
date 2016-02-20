d6908f3bd62b31381df116004a8284e3c047702a
NikolaMandic <mn080202@gmail.com>
2016-02-20 22:24:24 +0200

    vimrc from work

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,9 @@ git init
 git remote add origin https://github.com/NikolaMandic/houmdajrektori.git
 git fetch origin
 git checkout master
-git pull origin master
 git submodule update --init --recursive
 cd .vim/bundle/vimproc
 make
 cd ~
-source oh-my-zsh/tools/install.sh
+source .oh-my-zsh/tools/install.sh
 sudo chsh -s "$(command -v zsh)" "${USER}"
