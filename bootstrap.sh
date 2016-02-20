65adfa7fd4f8811c9ce6bacf01e4af03f1e4aa6c
NikolaMandic <mn080202@gmail.com>
2016-02-20 22:16:39 +0200

    vimrc from work

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 cd ~
-sudo apt-get install -y git build-essential
+sudo apt-get install -y git build-essential zsh
 git init
 git remote add origin https://github.com/NikolaMandic/houmdajrektori.git
 git fetch origin
 git checkout master
+git pull origin master
 git submodule update --init --recursive
 cd .vim/bundle/vimproc
 make
 cd ~
-source .ohmyzsh/tools/install.sh
-chsh -s "$(command -v zsh)" "${USER}"
+source oh-my-zsh/tools/install.sh
+sudo chsh -s "$(command -v zsh)" "${USER}"
